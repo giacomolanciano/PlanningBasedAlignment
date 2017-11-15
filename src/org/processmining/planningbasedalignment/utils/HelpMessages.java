e76b86b30844402e520b26b485b976b91800220f
glanciano <glanciano@a68f48d4-8d00-424f-b812-3f22a534a02a>
2017-11-15 16:30:25 +0000

    anonymization for ICAPS 2018
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/PlanningBasedAlignment/Trunk@37480 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/planningbasedalignment/utils/HelpMessages.java
+++ src/org/processmining/planningbasedalignment/utils/HelpMessages.java
@@ -2,15 +2,21 @@ package org.processmining.planningbasedalignment.utils;
 
 public class HelpMessages {
 
-	public static final String AFFILIATION = "Sapienza University of Rome";
-	public static final String AUTHOR = "Giacomo Lanciano";
-	public static final String EMAIL = "lanciano.1487019@studenti.uniroma1.it";
+	public static final String ANONYMIZED = "<anonymized for ICAPS 2018>";
+	
+//	public static final String AFFILIATION = "Sapienza University of Rome";
+//	public static final String AUTHOR = "Giacomo Lanciano";
+//	public static final String EMAIL = "lanciano.1487019@studenti.uniroma1.it";
+	public static final String AFFILIATION = ANONYMIZED;
+	public static final String AUTHOR = ANONYMIZED;
+	public static final String EMAIL = ANONYMIZED;
 	
 	public final static String PLANNING_BASED_ALIGNMENT_PACKAGE = "PlanningBasedAlignment";
 	
-	public final static String PLANNING_BASED_ALIGNMENT_ARTICLE = 
-		"'Aligning Real Process Executions and Prescriptive Process Models through Automated Planning' "
-		+ "by M. de Leoni and A. Marrella";
+//	public final static String PLANNING_BASED_ALIGNMENT_ARTICLE = 
+//		"'Aligning Real Process Executions and Prescriptive Process Models through Automated Planning' "
+//		+ "by M. de Leoni and A. Marrella";
+	public final static String PLANNING_BASED_ALIGNMENT_ARTICLE = ANONYMIZED;
 	
 	public final static String PLANNING_BASED_ALIGNMENT_HELP = ""
 		+ "Given an event log and a Petri net, this plug-in computes the (optimal) traces alignments using "
